817347c7d1c715722516350fda434ea1eb4057ec
MrCalogero <calogero27@hotmail.co.uk>
2016-07-13 22:06:36 +0100

    Fixed user email login error
    Fixed user email login error

--- register.php
+++ register.php
@@ -1,22 +1,29 @@
 <?php		
 include('app/init.php');
 $Template->set_data('page_class', 'register');
-	
-    if (isset($_REQUEST['submit']))
+if (isset($_REQUEST['submit']))
+{
+	extract($_REQUEST);
+	if ($upass != $upass2)
 	{
-        extract($_REQUEST);
-        $register = $User->reg_user($fullname, $uname, $upass, $uemail);
-        if ($register) 
+		# Incorrect Password 
+		$Template->set_alert('<p class="text-danger">The passwords inserted do not match. Please check the passwords and register again</p>');
+	}
+	else
+	{
+		$register = $User->reg_user($fullname, $uname, $upass, $uemail);
+		if ($register) 
 		{
-           # Successful Registration
-		   $Template->set_alert(' Registration  successful <a href="login.php">Click here</a>" to login');
-        } 
+			# Successful Registration
+			$Template->set_alert('<p class="text-success">Registration  successful <a href="login.php">Click here</a> to login</p>');
+		} 
 		else 
 		{
-           # Registration Failed
-		   $Template->set_alert('Registration failed. Email or Username already exits please try again');
-        }
+			# Registration Failed
+			$Template->set_alert('<p class="text-danger">Registration failed. Email or Username already exits please try again</p>');
+		}
+
 	}
+}
 
-	$Template->load('app/views/v_public_register.php');
-
+$Template->load('app/views/v_public_register.php');
